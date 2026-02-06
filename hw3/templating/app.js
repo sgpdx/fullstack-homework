aaf4ff0c263c7e8442df9bfaf264cc3953eeb7f0
Serena <sglick@pdx.edu>
2026-02-06 12:16:07 -0800

    Initial Outline of HW 3 with Pug

---
+++ hw3/templating/app.js
@@ -0,0 +1,46 @@
+// Initial code outline from https://github.com/caterinasworld/fullstack-examples/tree/main/express/09-pug
+// Edits and additional code by Serena Glick, February 2026
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
+// app.use(express.static('/public'));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Main",
+    heading: "Welcome to the Main Page to learn about the World",
+  });
+});
+
+app.get("/capitals", (req, res) => {
+  res.render("page", {
+    title: "Capitals",
+    heading: "Countries and Capitals",
+    subheading: "You may be surprised at what you learn!",
+  });
+});
+
+app.get("/populous", (req, res) => {
+  res.render("page", {
+    title: "Populous",
+    heading: "Most Populous Countries",
+    subheading: "Countries with a population of at least 50 million people",
+  });
+});
+
+app.get("/regions", (req, res) => {
+  res.render("page", {
+    title: "Regions",
+    heading: "Regions of the World",
+    subheading: "Number of countries in each region",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port http://localhost:${port}`);
+});
